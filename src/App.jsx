0ce5fcf0a32b1f82232dc65df6bfed9a44490180
Katherine <Evans.Katherine.E@gmail.com>
2023-04-16 14:08:59 -0500

    adds axios library an makes web request to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 function Header() {
   return (
     <header>
@@ -49,33 +51,20 @@ function Footer() {
 }
 
 function Content() {
-  let recipes = [
-    {
-      id: 1,
-      title: "Raw Eggs",
-      chef: "Peter Jang",
-      image_url: "https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg",
-    },
-    {
-      id: 2,
-      title: "Mud Pie",
-      chef: "Jay Wengrow",
-      image_url: "https://static.onecms.io/wp-content/uploads/sites/9/2017/12/mud-pie-XL-RECIPE2016.jpg",
-    },
-    {
-      id: 3,
-      title: "Pizza",
-      chef: "Jay Wengrow",
-      image_url:
-        "https://static.onecms.io/wp-content/uploads/sites/9/2021/06/15/mozzarella-pizza-margherita-FT-RECIPE0621.jpg",
-    },
-  ];
+  let recipes = [];
 
-  let cats = ["Tiger", "Charlie", "Rosie"];
+  const handleIndexRecipes = () => {
+    axios.get("http://localhost:3000/recipes.json").then(function (response) {
+      console.log(response);
+      recipes = response.data;
+    });
+  };
+
   return (
     <div>
       <RecipesNew />
-      <RecipesIndex recipes={recipes} myCats={cats} />
+      <button onClick={handleIndexRecipes}>Load Recipes</button>
+      <RecipesIndex recipes={recipes} />
     </div>
   );
 }
