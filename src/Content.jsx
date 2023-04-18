5d6734866cd9c18f7d9eb5117ba6cee08d630369
Katherine <Evans.Katherine.E@gmail.com>
2023-04-17 20:48:28 -0500

    adds functions to open/close modal

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,10 +2,12 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { RecipesNew } from "./RecipesNew";
 import { RecipesIndex } from "./RecipesIndex";
+import { Modal } from "./Modal";
 
 export function Content() {
   // ALLLLL JavaScript (NO HTML AT ALL)
   const [recipes, setRecipes] = useState([]);
+  const [isRecipesShowVisible, setIsRecipesShowVisible] = useState(false);
 
   const handleIndexRecipes = () => {
     axios.get("http://localhost:3000/recipes.json").then(function (response) {
@@ -14,13 +16,24 @@ export function Content() {
     });
   };
 
+  const handleShowRecipe = () => {
+    setIsRecipesShowVisible(true);
+  };
+
+  const handleClose = () => {
+    setIsRecipesShowVisible(false);
+  };
+
   useEffect(handleIndexRecipes, []);
 
   // HTML (CAN include some JavaScript too)
   return (
     <div>
       <RecipesNew />
-      <RecipesIndex recipes={recipes} />
+      <RecipesIndex recipes={recipes} onShowRecipe={handleShowRecipe} />
+      <Modal show={isRecipesShowVisible} onClose={handleClose}>
+        <p>test</p>
+      </Modal>
     </div>
   );
   // End HTML
