2f600117dadff8f09d73f98170430c199f210713
Katherine <Evans.Katherine.E@gmail.com>
2023-04-20 18:37:00 -0500

    updates the recipes new form to use javascript and axios

--- src/RecipesNew.jsx
+++ src/RecipesNew.jsx
@@ -1,13 +1,36 @@
+import axios from "axios";
+
 export function RecipesNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handleSubmit");
+    const params = new FormData(event.target);
+    axios.post("http://localhost:3000/recipes.json", params).then((response) => {
+      console.log(response);
+      event.target.reset();
+    });
+  };
   return (
     <div id="recipes-new">
       <h1>New recipe</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          Title: <input type="text" />
+          Title: <input name="title" type="text" />
         </div>
         <div>
-          Chef: <input type="text" />
+          Chef: <input name="chef" type="text" />
+        </div>
+        <div>
+          Ingredients: <input name="ingredients" type="text" />
+        </div>
+        <div>
+          Directions: <input name="directions" type="text" />
+        </div>
+        <div>
+          Prep Time: <input name="prep_time" type="text" />
+        </div>
+        <div>
+          Image URL: <input name="image_url" type="text" />
         </div>
         <button type="submit">Create recipe</button>
       </form>
