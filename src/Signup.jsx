90fa5f72a04d7696ef764ba7be4b0add92571ee6
Katherine <Evans.Katherine.E@gmail.com>
2023-04-25 18:36:02 -0500

    adds form validation for max name length

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -3,19 +3,20 @@ import { useState } from "react";
 
 export function Signup() {
   const [errors, setErrors] = useState([]);
+  const [name, setName] = useState("");
 
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault();
     setErrors([]);
     const params = new FormData(event.target);
     axios
       .post("http://localhost:3000/users.json", params)
-      .then((response) => {
+      .then(response => {
         console.log(response.data);
         event.target.reset();
         window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error.response.data.errors);
         setErrors(error.response.data.errors);
       });
@@ -25,24 +26,35 @@ export function Signup() {
     <div id="signup">
       <h1>Signup</h1>
       <ul>
-        {errors.map((error) => (
+        {errors.map(error => (
           <li key={error}>{error}</li>
         ))}
       </ul>
       <form onSubmit={handleSubmit}>
-        <div>
-          Name: <input name="name" type="text" />
+        <div className="form-group">
+          Name:
+          <input
+            className="form-control"
+            maxLength={20}
+            value={name}
+            onChange={event => setName(event.target.value)}
+            name="name"
+            type="text"
+          />
         </div>
-        <div>
-          Email: <input name="email" type="email" />
+        <small>{20 - name.length} characters remaining</small>
+        <div className="form-group">
+          Email: <input className="form-control" name="email" type="email" />
         </div>
-        <div>
-          Password: <input name="password" type="password" />
+        <div className="form-group">
+          Password: <input className="form-control" name="password" type="password" />
         </div>
-        <div>
-          Password confirmation: <input name="password_confirmation" type="password" />
+        <div className="form-group">
+          Password confirmation: <input className="form-control" name="password_confirmation" type="password" />
         </div>
-        <button type="submit">Signup</button>
+        <button type="submit" className="btn btn-secondary mt-3">
+          Signup
+        </button>
       </form>
     </div>
   );
