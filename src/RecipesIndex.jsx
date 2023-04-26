3c42b9b40bf54e8b750c72572c8a037b7a7e9cd9
Katherine <Evans.Katherine.E@gmail.com>
2023-04-25 20:33:09 -0500

    adds search bar to filter recipes

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -1,26 +1,40 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 export function RecipesIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
   return (
     <div id="recipes-index">
       <h1>All recipes</h1>
+      <div>
+        Search Recipes:{" "}
+        <input
+          value={searchFilter}
+          type="text"
+          onChange={event => {
+            setSearchFilter(event.target.value);
+          }}
+        />
+      </div>
       <div className="row">
-        {props.recipes.map((recipe) => (
-          <div key={recipe.id} className="recipes col mb-3">
-            <div className="card">
-              <img src={recipe?.image_url} className="card-img-top" />
-              <div className="card-body">
-                <h2 className="card-title">{recipe?.title}</h2>
-                <p className="card-text">{recipe?.chef}</p>
-                <button className="btn btn-primary" onClick={() => props.onShowRecipe(recipe)}>
-                  More Info!
-                </button>
-                <Link to={`/recipes/${recipe.id}`} className="btn btn-secondary">
-                  View Recipe
-                </Link>
+        {props.recipes
+          .filter(recipe => recipe.title.toLowerCase().includes(searchFilter.toLowerCase()))
+          .map(recipe => (
+            <div key={recipe.id} className="recipes col mb-3">
+              <div className="card">
+                <img src={recipe?.image_url} className="card-img-top" />
+                <div className="card-body">
+                  <h2 className="card-title">{recipe?.title}</h2>
+                  <p className="card-text">{recipe?.chef}</p>
+                  <button className="btn btn-primary" onClick={() => props.onShowRecipe(recipe)}>
+                    More Info!
+                  </button>
+                  <Link to={`/recipes/${recipe.id}`} className="btn btn-secondary">
+                    View Recipe
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
